65aa7726c2e5bac610107f43c5534910de356950
Jeremy Platt <jplatt4427@gmail.com>
2022-04-17 15:38:41 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainElem = document.getElementById("main");
+mainElem.remove();
+
+let newHeader = document.createElement("h1")
+newHeader.textContent = "Jeremy is the champion";
+newHeader.setAttribute("id", "victory");
+document.body.append(newHeader);
